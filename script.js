c034d36fa7f8f01447d603892742be0710736619
Aisha Mujahid <aisha.mujahid.kabir@gmail.com>
2025-08-31 21:20:18 +0100

    added async function

--- script.js
+++ script.js
@@ -26,3 +26,45 @@ function showPage(pageId) {
     document.getElementById(pageId).classList.add('active');
 }
 
+async function searchRecipes() {
+    const searchValue = searchInput.value.trim();
+    if (!searchValue) {
+        alert('Please enter a term');
+        return;
+    }
+    
+    const option = document.querySelector('input[name="option"]');
+    let apiSearch;
+
+    if(searchType === 'Recipe Name') {
+        apiSearch = "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata";
+    } else {
+        apiSearch = "www.themealdb.com/api/json/v1/1/list.php?c=list"
+    }
+    try{
+        const response = await fetch(apiSearch);
+        if (!response.ok){
+            throw new Error('No respnse');
+        }
+        const data = await response.json();
+
+        if (data.meals) {
+            currentRecipes = data.meals;
+            displayRecipes(data.meals);
+        } else {
+            recipesContainer.innerHTML = `
+            <div class="error-message">
+                <p>No recipes found! please try again</p>
+            </div>
+            `;
+        }
+    } catch (error) {
+        recipesContainer.innerHTML = `
+            <div class="error-message">
+                <p>No recipes found! please try again</p>
+            </div>
+            `;
+        console.error('Search error: ', error);
+    }
+
+}    
